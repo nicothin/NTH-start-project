696a464315cb957fe55410b89f04d658618b6d14
Nikolay Gromov <nicothin@gmail.com>
2016-02-22 00:24:09 +0300

    nice field file

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,42 @@
+
+'use strict';
+
+
+/*
+  Форма: работа стилизованного input[type="file"]
+  Автор: Osvaldas Valutis, www.osvaldas.info
+  Available for use under the MIT License
+*/
+
+;( function ( document, window, index )
+{
+  var inputs = document.querySelectorAll( '.field-file__input' );
+  Array.prototype.forEach.call( inputs, function( input )
+  {
+    var label  = input.nextElementSibling,
+        labelVal = label.innerHTML;
+
+    input.addEventListener( 'change', function( e ) {
+      var fileName = '';
+      if( this.files && this.files.length > 1 ) {
+        fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+      }
+      else {
+        fileName = e.target.value.split( '\\' ).pop();
+      }
+
+      if( fileName ) {
+        label.innerHTML = fileName;
+      }
+      else {
+        label.innerHTML = labelVal;
+      }
+    });
+  });
+}( document, window, 0 ));
+
+
+
 // $(document).ready(function() {
 //   alert('fuck jQuery');
 // });
