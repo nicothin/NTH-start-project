c613f0cb86a525652b7d2141a62ff5cb290f74fd
Nikolay Gromov <nicothin@gmail.com>
2016-03-25 16:39:31 +0300

    Библиотека: поправил описание Алертов

--- src/blocks/alert/alert__close.js
+++ src/blocks/alert/alert__close.js
@@ -1,10 +1,10 @@
 (function(){
-  // получим все закрывающих элементы в Алертах
+  // получим все закрывающих элементы в Сообщениях
   var alertCloseBtns = [].slice.call(document.querySelectorAll('.alert .alert__close') || []);
   // Навесим отслеживание событий клика
   alertCloseBtns.forEach(function(item) {
     item.addEventListener('click', function(){
-      // По клику поднимемся к родительскому Алерту и скроем его
+      // По клику поднимемся к родительскому Сообщению и скроем его
       this.closest('.alert').style.display = 'none';
     });
   });
