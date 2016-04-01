2eee67ee316eb8bb80e034c96c5273266aa40fb4
Nikolay Gromov <nicothin@gmail.com>
2016-04-01 10:33:31 +0300

    Переделка Навбара

--- src/blocks/navbar/navbar.js
+++ src/blocks/navbar/navbar.js
@@ -1,4 +1,6 @@
-// Переключение открытого/закрытого положений navbar
-document.getElementById('navbar-toggler').addEventListener('click', function(){
-  this.closest('.navbar').classList.toggle('navbar--open');
+$('#navbar-toggler').on('click', function(){
+  $(this).closest('.navbar')
+    .find('.navbar__slidable-content').slideToggle(250) // Показываем блок панели
+    .end()
+    .find('.navbar__toggler-icon').toggleClass('navbar__toggler-icon--open'); // Меняем вид иконки
 });
