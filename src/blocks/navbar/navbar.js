1d24f0e72671ed077dee7b6cbd24cc00c98cb585
Nikolay Gromov <nicothin@gmail.com>
2016-04-02 12:59:56 +0300

    Навбар (работа в процессе)

--- src/blocks/navbar/navbar.js
+++
@@ -1,6 +0,0 @@
-$('#navbar-toggler').on('click', function(){
-  $(this).closest('.navbar')
-    .find('.navbar__slidable-content').slideToggle(250) // Показываем блок панели
-    .end()
-    .find('.navbar__toggler-icon').toggleClass('navbar__toggler-icon--open'); // Меняем вид иконки
-});
