e96790ceada578870c83035ca4b86bebdab4eae5
Nikolay Gromov <nicothin@gmail.com>
2016-04-29 13:55:48 +0300

    Добавил кнопку прокрутки вверх

---
+++ src/blocks/to-top/to-top.js
@@ -0,0 +1,29 @@
+$( document ).ready(function() {
+
+  var t0;
+
+  $('#toTop').hide().on('click', function(event){
+    event.preventDefault();
+    $('body').animate({scrollTop:0}, 300);
+  });
+
+  $(window).on('scroll', function(){
+    clearTimeout(t0);
+    t0 = setTimeout(function () {
+        showScrollTopButton();
+        console.log('scroll');
+      }, 50);
+  });
+
+  showScrollTopButton();
+
+  function showScrollTopButton(){
+    if ( $(document).scrollTop() >= 500 ) {
+      $('#toTop').fadeIn();
+    }
+    else {
+      $('#toTop').fadeOut();
+    }
+  }
+
+});
