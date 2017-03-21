d2c21cf91c20b9d2ede68429b387f7f845026cb6
Nikolay Gromov <nicothin@gmail.com>
2017-03-21 22:22:04 +0300

    Таблицы со скроллом на малых вьюпортах

--- createBlock.js
+++ createBlock.js
@@ -58,7 +58,7 @@ if(blockName) {
 
         // Если это HTML
         else if(extention == 'html') {
-          fileContent = '<!--DEV\n\nДля использования этого файла как шаблона:\n\n@ @include(\'blocks/' + blockName + '/' + blockName + '.html\')\n\n(Нужно убрать пробел между символами @)\nПодробнее: https://www.npmjs.com/package/gulp-file-include-->\n\n<div class="' + blockName + '">content</div>\n';
+          fileContent = '<!--DEV\n\nДля использования этого файла как шаблона:\n\n@ @include(\'blocks/' + blockName + '/' + blockName + '.html\')\n\n(Нужно убрать пробел между символами @)\nПодробнее: https://www.npmjs.com/package/gulp-file-include\n\n-->\n\n<div class="' + blockName + '">content</div>\n';
           // fileCreateMsg = '';
         }
 
