0b8d88439cd26549581c2a670dfd02f4ae8f6519
Nikolay Gromov <nicothin@gmail.com>
2017-03-22 21:57:18 +0300

    Добавил owl-carousel

---
+++ src/blocks/owl-carousel/owl-carousel.js
@@ -0,0 +1,33 @@
+// $(document).ready(function(){
+
+//   $("#owl-carousel-demo").owlCarousel({
+//     items: 3,
+//     nav: true,
+//     loop: true,
+//     center: true,
+//     responsive : {
+//       0 : {
+//         items: 1,
+//         nav: true,
+//         loop: true,
+//         center: true,
+//       },
+//       480 : {
+//         items: 3,
+//       },
+//       768 : {
+//         items: 3,
+//       },
+//       992 : {
+//         items: 3,
+//       },
+//       1200 : {
+//         items: 3,
+//       },
+//       1800 : {
+//         items: 3,
+//       }
+//     }
+//   });
+
+// });
