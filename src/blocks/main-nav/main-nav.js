76729f34dd67022abd259b23a61d0addfb3a04d9
Nikolay Gromov <nicothin@gmail.com>
2017-03-24 15:10:00 +0300

    Подправил JS для гл. меню

--- src/blocks/main-nav/main-nav.js
+++ src/blocks/main-nav/main-nav.js
@@ -2,12 +2,14 @@
 
   var toggler = document.getElementById('main-nav-toggler');
 
-  toggler.addEventListener('click', mainNavVisibleToggle);
+  if(toggler){
+    toggler.addEventListener('click', mainNavVisibleToggle);
 
-  function mainNavVisibleToggle(e) {
-    e.preventDefault();
-    toggler.classList.toggle('main-nav__toggler--close');
-    document.getElementById('main-nav').classList.toggle('main-nav--open');
+    function mainNavVisibleToggle(e) {
+      e.preventDefault();
+      toggler.classList.toggle('main-nav__toggler--close');
+      document.getElementById('main-nav').classList.toggle('main-nav--open');
+    }
   }
 
 }());
