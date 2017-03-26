635b816fa64973b37821976e8c4f4a1202088a4e
Nikolay Gromov <nicothin@gmail.com>
2017-03-26 19:41:53 +0300

    Прокачал main-menu для работы с клавиатуры

--- src/blocks/main-nav/main-nav.js
+++ src/blocks/main-nav/main-nav.js
@@ -1,7 +1,7 @@
 (function(){
 
+  // Добавление/удаление модификаторов при клике на переключение видимости
   var toggler = document.getElementById('main-nav-toggler');
-
   if(toggler){
     toggler.addEventListener('click', mainNavVisibleToggle);
 
@@ -11,5 +11,65 @@
       document.getElementById('main-nav').classList.toggle('main-nav--open');
     }
   }
+
+  // Добавление/удаление модификаторов при фокусировке на ссылочном элементе
+  var linkClassName = 'main-nav__link';
+  var linkClassNameShowChild = 'main-nav__item--show-child';
+  var findLinkClassName = new RegExp(linkClassName);
+  // Слежение за всплывшим событием focus
+  document.addEventListener('focus', function(event) {
+    // Если событие всплыло от одной из ссылок гл. меню
+    if (findLinkClassName.test(event.target.className)) {
+      // Добавим классы, показывающие списки вложенных уровней, на всех родителей
+      event.target.parents('.main-nav__item').forEach(function(item){
+        item.classList.add(linkClassNameShowChild);
+      });
+    }
+  }, true);
+  // Слежение за всплывшим событием blur
+  document.addEventListener('blur', function(event) {
+    // Если событие всплыло от одной из ссылок гл. меню
+    if (findLinkClassName.test(event.target.className)) {
+      // Уберем все классы, показывающие списки 2+ уровней
+      // event.target.closest('.main-nav').querySelectorAll('.'+linkClassNameShowChild).forEach(function(item){
+      document.querySelectorAll('.'+linkClassNameShowChild).forEach(function(item){
+        item.classList.remove(linkClassNameShowChild);
+      });
+    }
+  }, true);
+
+
+
+  // Добавление метода .parents()
+  Element.prototype.parents = function(selector) {
+    var elements = [];
+    var elem = this;
+    var ishaveselector = selector !== undefined;
+
+    while ((elem = elem.parentElement) !== null) {
+      if (elem.nodeType !== Node.ELEMENT_NODE) {
+        continue;
+      }
+
+      if (!ishaveselector || elem.matches(selector)) {
+        elements.push(elem);
+      }
+    }
+
+    return elements;
+  };
+
+  // Добавление метода .closest() (полифил, собственно)
+  // (function(e){
+  //  e.closest = e.closest || function(css){
+  //    var node = this;
+
+  //    while (node) {
+  //       if (node.matches(css)) return node;
+  //       else node = node.parentElement;
+  //    }
+  //    return null;
+  //  }
+  // })(Element.prototype);
 
 }());
