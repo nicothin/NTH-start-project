83df2894fe91d846e0be0497a41c491460bac070
Nikolay Gromov <nicothin@gmail.com>
2017-04-02 01:02:51 +0300

    Допилил табы

--- src/blocks/tabs/tabs.js
+++ src/blocks/tabs/tabs.js
@@ -1,22 +1,51 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  var tabsParentClassName = 'tabs';
-  var shownTabClassName = 'tabs__content-item--active';
+  if(location.hash) {
+    showTab(location.hash);
+  }
 
+  // Следим за поднимающимися кликами
   document.addEventListener('click', function(event) {
     if(event.target.dataset.toggle === 'tab') {
       event.preventDefault();
       var target = event.target.hash === undefined ? event.target.dataset.target : event.target.hash;
-      // console.log( target );
       if ( target !== undefined ) {
-        var tabsParent = document.querySelector(target).closest('.'+tabsParentClassName);
-        tabsParent.querySelectorAll('.'+shownTabClassName).forEach(function(item){
-          item.classList.remove(shownTabClassName);
-        });
-        tabsParent.querySelector(target).classList.add(shownTabClassName);
+        showTab(target);
+        if(history && history.pushState && history.replaceState) {
+          var stateObject = {'url' : target};
+          if (window.location.hash && stateObject.url !== window.location.hash) {
+            window.history.pushState(stateObject, document.title, window.location.pathname + target);
+          } else {
+            window.history.replaceState(stateObject, document.title, window.location.pathname + target);
+          }
+        }
       }
     }
   });
+
+  /**
+   * Показывает таб
+   * @param  {string} tabId ID таба, который нужно показать
+   */
+  function showTab(tabId){
+    var element = document.querySelector(tabId);
+    if ( element && element.classList.contains('tabs__content-item') ) {
+      var tabsParent = document.querySelector(tabId).closest('.tabs');
+      var activeTabClassName = 'tabs__link-wrap--active';
+      var activeTabContentClassName = 'tabs__content-item--active';
+      // таб
+      tabsParent.querySelectorAll('.'+activeTabClassName).forEach(function(item){
+        item.classList.remove(activeTabClassName);
+      });
+      var activeTab = tabsParent.querySelector('[href="'+tabId+'"]') ? tabsParent.querySelector('[href="'+tabId+'"]') : tabsParent.querySelector('[data-target="'+tabId+'"]')
+      activeTab.closest('.tabs__link-wrap').classList.add(activeTabClassName);
+      // контент таба
+      tabsParent.querySelectorAll('.'+activeTabContentClassName).forEach(function(item){
+        item.classList.remove(activeTabContentClassName);
+      });
+      tabsParent.querySelector(tabId).classList.add(activeTabContentClassName);
+    }
+  }
 
   // Добавление метода .closest() (полифил, собственно)
   (function(e){
