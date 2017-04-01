10e3ac63c9edea4c2a9faadbd4d6852c43575dc5
Nikolay Gromov <nicothin@gmail.com>
2017-04-01 23:22:36 +0300

    Табы работоспособны, но не меняют и не реагируют на hash

--- src/blocks/tabs/tabs.js
+++ src/blocks/tabs/tabs.js
@@ -1,16 +1,34 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  // в процессе разработки
+  var tabsParentClassName = 'tabs';
+  var shownTabClassName = 'tabs__content-item--active';
 
   document.addEventListener('click', function(event) {
     if(event.target.dataset.toggle === 'tab') {
       event.preventDefault();
       var target = event.target.hash === undefined ? event.target.dataset.target : event.target.hash;
-      console.log( target );
+      // console.log( target );
       if ( target !== undefined ) {
-        console.log( document.querySelector(target) );
+        var tabsParent = document.querySelector(target).closest('.'+tabsParentClassName);
+        tabsParent.querySelectorAll('.'+shownTabClassName).forEach(function(item){
+          item.classList.remove(shownTabClassName);
+        });
+        tabsParent.querySelector(target).classList.add(shownTabClassName);
       }
     }
   });
+
+  // Добавление метода .closest() (полифил, собственно)
+  (function(e){
+   e.closest = e.closest || function(css){
+     var node = this;
+
+     while (node) {
+        if (node.matches(css)) return node;
+        else node = node.parentElement;
+     }
+     return null;
+   }
+  })(Element.prototype);
 
 });
