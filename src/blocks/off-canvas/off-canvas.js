ec0d9e3d1cc034c203527279230eb936d03d9a7e
Nikolay Gromov <nicothin@gmail.com>
2017-06-09 01:19:47 +0300

    Закомментировал свайп оффканваса

--- src/blocks/off-canvas/off-canvas.js
+++ src/blocks/off-canvas/off-canvas.js
@@ -12,45 +12,45 @@ document.addEventListener('DOMContentLoaded', function(){
   });
 
   // реакция на свайп
-  document.addEventListener('touchstart', handleTouchStart, false);
-  document.addEventListener('touchmove', handleTouchMove, false);
-  var xDown = null;
-  var yDown = null;
-  function handleTouchStart(evt) {
-    xDown = evt.touches[0].clientX;
-    yDown = evt.touches[0].clientY;
-  };
-  function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-      return;
-    }
+  // document.addEventListener('touchstart', handleTouchStart, false);
+  // document.addEventListener('touchmove', handleTouchMove, false);
+  // var xDown = null;
+  // var yDown = null;
+  // function handleTouchStart(evt) {
+  //   xDown = evt.touches[0].clientX;
+  //   yDown = evt.touches[0].clientY;
+  // };
+  // function handleTouchMove(evt) {
+  //   if ( ! xDown || ! yDown ) {
+  //     return;
+  //   }
 
-    var xUp = evt.touches[0].clientX;
-    var yUp = evt.touches[0].clientY;
+  //   var xUp = evt.touches[0].clientX;
+  //   var yUp = evt.touches[0].clientY;
 
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-    if(Math.abs( xDiff )+Math.abs( yDiff )>100){ //to deal with to short swipes
+  //   var xDiff = xDown - xUp;
+  //   var yDiff = yDown - yUp;
+  //   if(Math.abs( xDiff )+Math.abs( yDiff )>100){ //to deal with to short swipes
 
-      if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {/* left swipe */
-          document.getElementById('off-canvas').classList.remove('off-canvas--open');
-        } else {/* right swipe */
-          document.getElementById('off-canvas').classList.add('off-canvas--open');
-        }
-      }
-      // else {
-      //   if ( yDiff > 0 ) {/* up swipe */
-      //     alert('Up!');
-      //   } else { /* down swipe */
-      //     alert('Down!');
-      //   }
-      // }
-      /* reset values */
-      xDown = null;
-      yDown = null;
-    }
-  };
+  //     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+  //       if ( xDiff > 0 ) {/* left swipe */
+  //         document.getElementById('off-canvas').classList.remove('off-canvas--open');
+  //       } else {/* right swipe */
+  //         document.getElementById('off-canvas').classList.add('off-canvas--open');
+  //       }
+  //     }
+  //     // else {
+  //     //   if ( yDiff > 0 ) {/* up swipe */
+  //     //     alert('Up!');
+  //     //   } else { /* down swipe */
+  //     //     alert('Down!');
+  //     //   }
+  //     // }
+  //     /* reset values */
+  //     xDown = null;
+  //     yDown = null;
+  //   }
+  // };
 
   function offCanvasToggle() {
     document.getElementById('off-canvas').classList.toggle('off-canvas--open');
