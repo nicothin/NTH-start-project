836798640ce905b2928813ae351127b87055d2e6
Nikolay Gromov <nicothin@gmail.com>
2017-07-31 01:29:22 +0300

    Update createBlock.js

--- createBlock.js
+++ createBlock.js
@@ -35,7 +35,7 @@ if (blockName) {
 
         // Если это SCSS
         if (extention === 'scss') {
-          fileContent = `// В этом файле должны быть стили для БЭМ-блока ${blockName}, его элементов, \n// модификаторов, псевдоселекторов, псевдоэлементов, @media-условий...\n// Очередность: http://nicothin.github.io/idiomatic-pre-CSS/#priority\n\n.${blockName} {\n\n  $block-name:                &;\n  // #{$block-name}__element {}\n\n}\n`;
+          fileContent = `// В этом файле должны быть стили для БЭМ-блока ${blockName}, его элементов, \n// модификаторов, псевдоселекторов, псевдоэлементов, @media-условий...\n// Очередность: http://nicothin.github.io/idiomatic-pre-CSS/#priority\n\n.${blockName} {\n\n  $block-name:                &; // #{$block-name}__element {}\n\n}\n`;
           // fileCreateMsg = '';
 
           // Добавим созданный файл
