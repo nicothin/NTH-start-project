5d0b87a941f8ba70d96ca518243dd3f1230024b7
Nikolay Gromov <nicothin@gmail.com>
2017-10-28 18:44:42 +0300

    Рефакторинг глобального JS-файла

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,3 +1,22 @@
+// Если на проекте jQuery
 // $( document ).ready(function() {
+//   // code
+// });
 
+// Изоляция без jQuery
+// (function(){
+//   // code
+// }());
+
+// На проекте нет jQuery, но хочется $( document ).ready...
+// function ready(fn) {
+//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+//     fn();
+//   } else {
+//     document.addEventListener('DOMContentLoaded', fn);
+//   }
+// }
+//
+// ready(function(){
+//   // code
 // });
