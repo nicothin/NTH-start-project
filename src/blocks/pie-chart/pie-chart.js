ce33954196a6d4c5f707153d6e99158c58173b51
Nikolay Gromov <nicothin@gmail.com>
2017-11-17 15:12:17 +0300

    Добавил круговые диаграммы

---
+++ src/blocks/pie-chart/pie-chart.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  // Автор кода — Лия Веру
+
+  function $$(selector, context) {
+    context = context || document;
+    var elements = context.querySelectorAll(selector);
+    return Array.prototype.slice.call(elements);
+  }
+
+  $$('.pie-chart').forEach(function(pie) {
+    var p = pie.textContent;
+    pie.style.animationDelay = '-' + parseFloat(p) + 's';
+  });
+
+});
