9a4617e8f61d0bbb710a49434a408677a627f0ef
Nikolay Gromov <nicothin@gmail.com>
2018-02-13 22:27:00 +0300

    Исправления JS для случаев отсутствия разметки JS-компонентов

--- src/blocks/swipe/swipe.js
+++ src/blocks/swipe/swipe.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  window.mySwipe = new Swipe(document.getElementById('swipe-demo'), {
-    startSlide: 0,
-    speed: 400,
-    auto: 3000,
-    draggable: true,
-    continuous: true,
-    disableScroll: true,
-    stopPropagation: true,
-    callback: function(index, elem, dir) {},
-    transitionEnd: function(index, elem) {}
-  });
+  if(document.getElementById('swipe-demo')) {
 
-  // включить видимость блока (по умолчанию скрыт)
-  document.getElementById('swipe-demo').classList.add('swipe--enable');
+    window.mySwipe = new Swipe(document.getElementById('swipe-demo'), {
+      startSlide: 0,
+      speed: 400,
+      auto: 3000,
+      draggable: true,
+      continuous: true,
+      disableScroll: true,
+      stopPropagation: true,
+      callback: function(index, elem, dir) {},
+      transitionEnd: function(index, elem) {}
+    });
 
+    // включить видимость блока (по умолчанию скрыт)
+    document.getElementById('swipe-demo').classList.add('swipe--enable');
+
+  }
 });
--- src/blocks/to-top/to-top.js
+++ src/blocks/to-top/to-top.js
@@ -1,48 +1,52 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  document.querySelector('#to-top').addEventListener('click', function(e) {
-    e.preventDefault();
-    var scroll = window.pageYOffset;
-    var targetTop = 0;
-    var scrollDiff = (scroll - targetTop) * -1;
-    animate({
-      duration: 500,
-      timing: function(timeFraction) {
-        return Math.pow(timeFraction, 4); // https://learn.javascript.ru/js-animation
-      },
-      draw: function(progress) {
-        var scrollNow = scroll + progress * scrollDiff;
-        window.scrollTo(0,scrollNow);
+  if(document.getElementById('to-top')) {
+
+    document.getElementById('to-top').addEventListener('click', function(e) {
+      e.preventDefault();
+      var scroll = window.pageYOffset;
+      var targetTop = 0;
+      var scrollDiff = (scroll - targetTop) * -1;
+      animate({
+        duration: 500,
+        timing: function(timeFraction) {
+          return Math.pow(timeFraction, 4); // https://learn.javascript.ru/js-animation
+        },
+        draw: function(progress) {
+          var scrollNow = scroll + progress * scrollDiff;
+          window.scrollTo(0,scrollNow);
+        }
+      });
+    }, false);
+
+    window.addEventListener('scroll', visibilityToggle);
+    visibilityToggle();
+
+    function visibilityToggle() {
+      if(window.pageYOffset >= 500) {
+        document.getElementById('to-top').classList.add('to-top--visible');
       }
-    });
-  }, false);
-
-  window.addEventListener('scroll', visibilityToggle);
-  visibilityToggle();
-
-  function visibilityToggle() {
-    if(window.pageYOffset >= 500) {
-      document.querySelector('#to-top').classList.add('to-top--visible');
-    }
-    else {
-      document.querySelector('#to-top').classList.remove('to-top--visible');
-    }
-  }
-
-  function animate(_ref) {
-    var timing = _ref.timing,
-        draw = _ref.draw,
-        duration = _ref.duration;
-    var start = performance.now();
-    requestAnimationFrame(function animate(time) {
-      var timeFraction = (time - start) / duration;
-      if (timeFraction > 1) timeFraction = 1;
-      var progress = timing(timeFraction);
-      draw(progress);
-      if (timeFraction < 1) {
-        requestAnimationFrame(animate);
+      else {
+        document.getElementById('to-top').classList.remove('to-top--visible');
       }
-    });
+    }
+
+    function animate(_ref) {
+      var timing = _ref.timing,
+          draw = _ref.draw,
+          duration = _ref.duration;
+      var start = performance.now();
+      requestAnimationFrame(function animate(time) {
+        var timeFraction = (time - start) / duration;
+        if (timeFraction > 1) timeFraction = 1;
+        var progress = timing(timeFraction);
+        draw(progress);
+        if (timeFraction < 1) {
+          requestAnimationFrame(animate);
+        }
+      });
+    }
+
   }
 
 });
