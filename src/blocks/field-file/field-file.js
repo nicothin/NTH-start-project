3d66a8893171d89888a4e9956ad03704c1ffa9ad
Nikolay Gromov <nicothin@gmail.com>
2019-01-04 15:37:34 +0300

    Рефакторинг: field-file

--- src/blocks/field-file/field-file.js
+++ src/blocks/field-file/field-file.js
@@ -1,3 +1,5 @@
+const closest = require('closest');
+
 /*
   Форма: работа стилизованного input[type="file"]
   Автор: Osvaldas Valutis, www.osvaldas.info (адаптировано под используемую разметку)
@@ -6,32 +8,6 @@
 
 ;( function ()
 {
-  function closest(el, selector) {
-    var matchesFn;
-
-    // find vendor prefix
-    ['matches','webkitMatchesSelector','mozMatchesSelector','msMatchesSelector','oMatchesSelector'].some(function(fn) {
-      if (typeof document.body[fn] == 'function') {
-        matchesFn = fn;
-        return true;
-      }
-      return false;
-    })
-
-    var parent;
-
-    // traverse parents
-    while (el) {
-      parent = el.parentElement;
-      if (parent && parent[matchesFn](selector)) {
-        return parent;
-      }
-      el = parent;
-    }
-
-    return null;
-  }
-
   var inputs = document.querySelectorAll( '.field-file__input' );
   Array.prototype.forEach.call( inputs, function( input )
   {
