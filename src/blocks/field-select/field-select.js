657959e72a3d1e0064971dc30d9aa2f03c0c4523
Nikolay Gromov <nicothin@gmail.com>
2019-01-05 14:43:02 +0300

    Допилил поддержку IE в кастомных селектах

--- src/blocks/field-select/field-select.js
+++ src/blocks/field-select/field-select.js
@@ -1,3 +1,25 @@
+if (typeof Object.assign != 'function') {
+  Object.assign = function(target) {
+    'use strict';
+    if (target == null) {
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    target = Object(target);
+    for (var index = 1; index < arguments.length; index++) {
+      var source = arguments[index];
+      if (source != null) {
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+    }
+    return target;
+  };
+}
+
 const Choices = require('choices.js');
 
 const choices = new Choices('.field-select__select', {
