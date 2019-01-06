fe88f97dba5e4e7ed922bcc452c6c37ed4f25783
Nikolay Gromov <nicothin@gmail.com>
2019-01-06 23:10:01 +0300

    Глобальный JS-файл: подпилил заготовку

---
+++ src/js/documentReady.js
@@ -0,0 +1,11 @@
+/* global document module */
+
+const ready = function (fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+module.exports = ready;
--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,8 @@
-// Если на проекте jQuery
-// var $ = require('jquery');
-// $( document ).ready(function() {
-//   // code
-// });
+// var ready = require('./documentReady.js');
 
-// Изоляция без jQuery
-// (function(){
-//   // code
-// }());
-
-// На проекте нет jQuery, но хочется $( document ).ready...
-// function ready(fn) {
-//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
-//   }
-// }
-//
 // ready(function(){
-//   // code
+//   console.log('DOM героически построен!');
 // });
 
-// Взять целевой файл
-// require('./some-file.js');
+// var $ = require('jquery');
+// $( document ).ready(function() {});
