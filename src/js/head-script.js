9f002385cedce08cd5b12cd9cc2e33051fb1e397
Nikolay Gromov <nicothin@gmail.com>
2019-02-03 10:29:01 +0300

    Инлайновый JS в head: добавил детектирование ОС

--- src/js/head-script.js
+++ src/js/head-script.js
@@ -6,3 +6,6 @@ if(/constructor/i.test(window.HTMLElement)||(function(p){return p.toString()==="
 if(/*@cc_on!@*/false||!!document.documentMode)cth('ie');
 if(!(/*@cc_on!@*/false||!!document.documentMode)&&!!window.StyleMedia)cth('edge');
 if(!!window.chrome&&(!!window.chrome.webstore||!!window.chrome.runtime))cth('chrome');
+if(~navigator.appVersion.indexOf("Win"))cth('windows');
+if(~navigator.appVersion.indexOf("Mac"))cth('osx');
+if(~navigator.appVersion.indexOf("Linux"))cth('linux');
