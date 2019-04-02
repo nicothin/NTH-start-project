7b4ca7bfeed3de7fcc81a21127e83ccd4f9adc50
Nikolay Gromov <nicothin@gmail.com>
2019-04-02 14:07:53 +0300

    Добавил в описание инструкцию по работе со сторонними модулями

--- config.js
+++ config.js
@@ -1,38 +1,41 @@
 /* global module */
 
 let config = {
-  "notGetBlocks": [
-    "blocks-demo.html",
+  'notGetBlocks': [
+    'blocks-demo.html',
   ],
-  "ignoredBlocks": [
-    "no-js",
+  'ignoredBlocks': [
+    'no-js',
   ],
-  "alwaysAddBlocks": [
-    // "sprite-svg",
-    // "sprite-png",
-    // "object-fit-polyfill",
+  'alwaysAddBlocks': [
+    // 'sprite-svg',
+    // 'sprite-png',
+    // 'object-fit-polyfill',
   ],
-  "addStyleBefore": [
-    "src/scss/variables.scss",
-    "src/scss/mixins.scss",
-    // "somePackage/dist/somePackage.css", // для "node_modules/somePackage/dist/somePackage.css",
+  'addStyleBefore': [
+    'src/scss/variables.scss',
+    'src/scss/mixins.scss',
+    // 'somePackage/dist/somePackage.css', // для 'node_modules/somePackage/dist/somePackage.css',
   ],
-  "addStyleAfter": [
-    // "src/scss/print.scss",
+  'addStyleAfter': [
+    // 'src/scss/print.scss',
   ],
-  "addJsBefore": [],
-  "addJsAfter": [
-    "./script.js",
+  'addJsBefore': [
+    // 'somePackage/dist/somePackage.js', // для 'node_modules/somePackage/dist/somePackage.js',
   ],
-  "addAssets": {
-    "src/fonts/demo-empty-open-sans.woff2": "fonts/",
-    "src/img/demo-*.{png,svg,jpg,jpeg}": "img/",
-    // "src/favicon/*.{png,ico,svg,xml,webmanifest}": "img/favicon",
+  'addJsAfter': [
+    './script.js',
+  ],
+  'addAssets': {
+    'src/fonts/demo-empty-open-sans.woff2': 'fonts/',
+    'src/img/demo-*.{png,svg,jpg,jpeg}': 'img/',
+    // 'src/favicon/*.{png,ico,svg,xml,webmanifest}': 'img/favicon',
+    // 'node_modules/somePackage/images/*.{png,svg,jpg,jpeg}': 'img/',
   },
-  "dir": {
-    "src": "src/",
-    "build": "build/",
-    "blocks": "src/blocks/"
+  'dir': {
+    'src': 'src/',
+    'build': 'build/',
+    'blocks': 'src/blocks/'
   }
 };
 
